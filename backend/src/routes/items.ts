17734cc77c956f97b779b92804ad9240f8528010
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-11-21 16:42:23 +0300

    Create items.ts

---
+++ backend/src/routes/items.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", async (_req: express.Request, res: express.Response) => {
+    res.send("Fetching all items...");
+});
+
+router.post("/", async (_req: express.Request, res: express.Response) => {
+   res.send("Adding new item...");
+});
+
+export default router;
