2effdc27fc890355fa7ff35b1c73444df5b46193
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-11-21 21:44:44 +0300

    Configure sequelize

---
+++ backend/src/utils/db.ts
@@ -0,0 +1,29 @@
+import sequelize, {Sequelize} from "sequelize";
+
+const {
+    DATABASE_HOST,
+    DATABASE_PASSWORD,
+    DATABASE_USER,
+    DATABASE_NAME,
+    DATABASE_PORT
+} = require("./config");
+
+const sequelize = new Sequelize(
+    `postgresql://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`, {
+        dialect: "postgres",
+        logging: false
+    }
+);
+
+const connectToDatabase = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Connected to database");
+    } catch (error) {
+        console.error(error);
+        return process.exit(1);
+    }
+    return null;
+}
+
+export default { connectToDatabase, sequelize };
