97234de58b94b661972bcadeb7464389e4607ae3
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-11-23 00:03:17 +0300

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import app from "./app";
+import connectToDatabase from "./src/utils/db"
 import { PORT } from "./src/utils/config";
 
 app.use(express.json());
 
-app.listen(PORT, (): void => {
-    console.log(`Server started on port ${PORT}`);
-});
+
+const start = async () => {
+    await connectToDatabase();
+    app.listen(PORT, (): void => {
+        console.log(`Server started on port ${PORT}`);
+    });
+}
+
+start();
