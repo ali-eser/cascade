28402c27711c83fd54331a53c3ae910b87bc75d0
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-11-22 04:00:29 +0300

    Update items.ts

--- backend/src/routes/items.ts
+++ backend/src/routes/items.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-const router = express.Router();
+const router: express.Router = express.Router();
 
 router.get("/", async (_req: express.Request, res: express.Response) => {
     res.send("Fetching all items...");
