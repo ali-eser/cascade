2096a8b3acc5ef46ba05ff44d0ba18cb1e2c1de1
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-11-22 04:00:26 +0300

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import app from "./app";
+
 app.use(express.json());
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, (): void => {
     console.log(`Server started on port ${PORT}`);
 });
