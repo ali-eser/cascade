47df5fc1c297ca960b3b5d173cde16d560d09a82
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-11-25 18:42:12 +0300

    Update users.ts

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,13 +1,27 @@
 import express from "express";
+import userService from "../services/users";
 
-const router: express.Router = express.Router();
+const usersRouter: express.Router = express.Router();
 
-router.get("/", async (_req: express.Request, res: express.Response) => {
-    res.send("Fetching all users...");
+usersRouter.get("/", async (_req: express.Request, res: express.Response) => {
+    try {
+        const users = await userService.getUsers();
+        res.status(200).json(users);
+    } catch (e) {
+        res.status(500).json({ e });
+    }
 });
 
-router.post("/", async (_req: express.Request, res: express.Response) => {
-   res.send("Creating new user...");
+usersRouter.post("/", async (req: express.Request, res: express.Response) => {
+    try {
+        const { username, email, name, password } = req.body;
+        const newUser = await userService.addUser(username, email, name, password);
+
+        res.status(201).json(newUser);
+    } catch (e) {
+        res.status(500).json({ e });
+    }
+
 });
 
-export default router;
+export default usersRouter;
