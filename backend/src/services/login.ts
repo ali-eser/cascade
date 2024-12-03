b8cd40f6079f3bb7338fdc74916df5194dd57d2e
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-12-04 02:11:17 +0300

    Update login.ts

--- backend/src/services/login.ts
+++ backend/src/services/login.ts
@@ -3,29 +3,29 @@ import bcrypt from "bcrypt";
 import {User} from "../utils/db";
 
 const login = async (username: string, password: string) => {
-    try {
-        const user = await User.findOne({ where: { username: username } });
+  try {
+    const user = await User.findOne({ where: { username: username } });
 
-        const isCorrect = user === null
-            ? false
-            : await bcrypt.compare(password, user.passwordHash)
+    const isCorrect = user === null
+      ? false
+      : await bcrypt.compare(password, user.passwordHash)
 
-        if (!(user && isCorrect)) {
-            return false;
-        }
-
-        const userToLogin = {
-            username: user.username,
-            id: user.id
-        };
-
-        const token = jwt.sign(userToLogin, process.env.SECRET || "default_secret_key");
-
-        return { token, user: username, id: user.id };
-    } catch (e) {
-        console.error(e);
-        return 0;
+    if (!(user && isCorrect)) {
+      return false;
     }
+
+    const userToLogin = {
+      username: user.username,
+      id: user.id
+    };
+
+    const token = jwt.sign(userToLogin, process.env.SECRET || "default_secret_key");
+
+    return { token, user: username, id: user.id };
+  } catch (e) {
+    console.error(e);
+    return 0;
+  }
 };
 
 export default { login };
