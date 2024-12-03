d760cc9e15a5ebd17ec730b4c84808114a45f360
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-12-04 02:46:35 +0300

    Update app.ts

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import loginRouter from "./src/routes/login";
 import itemsRouter from "./src/routes/items";
 import usersRouter from "./src/routes/users";
 
 const app = express();
 
+app.use(express.json());
+
+app.use("/api/login", loginRouter);
 app.use("/api/items", itemsRouter);
 app.use("/api/users", usersRouter);
 
