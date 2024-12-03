0be366777a89072ec76963e716e57e7ede52bcb9
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-12-04 02:11:06 +0300

    Update user.ts

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,43 +1,43 @@
 import { Model, DataType, Table, Column } from 'sequelize-typescript';
 
 @Table({
-    tableName: 'users',
-    timestamps: true,
-    underscored: true
+  tableName: 'users',
+  timestamps: true,
+  underscored: true
 })
 class User extends Model {
-    @Column({
-        primaryKey: true,
-        type: DataType.INTEGER,
-        autoIncrement: true,
-    })
-    id!: number;
+  @Column({
+    primaryKey: true,
+    type: DataType.INTEGER,
+    autoIncrement: true,
+  })
+  id!: number;
 
-    @Column({
-        type: DataType.STRING,
-        allowNull: false,
-        unique: true
-    })
-    username!: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    unique: true
+  })
+  username!: string;
 
-    @Column({
-        type: DataType.STRING,
-        allowNull: false,
-        unique: true
-    })
-    email!: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    unique: true
+  })
+  email!: string;
 
-    @Column({
-        type: DataType.STRING,
-        allowNull: false
-    })
-    name!: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false
+  })
+  name!: string;
 
-    @Column({
-        type: DataType.STRING,
-        allowNull: false
-    })
-    passwordHash!: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false
+  })
+  passwordHash!: string;
 }
 
 export default User;
