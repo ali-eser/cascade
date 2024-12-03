82ac09af5b1a28ad2d97895bacb709bedac76140
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-12-04 02:27:43 +0300

    Implement loginService

---
+++ frontend/src/services/loginService.ts
@@ -0,0 +1,16 @@
+const baseURL = process.env.BASE_URL;
+
+type UserToLogin = { username: string; password: string };
+
+const login = async (credentials: UserToLogin)=> {
+  const { username, password } = credentials;
+
+  const res = await fetch(`${baseURL}/api/login`, {
+    method: "POST",
+    body: JSON.stringify({ username, password }),
+  });
+
+  return res.json();
+}
+
+export default { login };
