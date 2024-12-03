794593fceabebd6197d4b3dff56cd8c664ac42cb
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-12-04 02:47:19 +0300

    Throw AuthenticationError on invalid creds

--- backend/src/services/login.ts
+++ backend/src/services/login.ts
@@ -2,30 +2,32 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import {User} from "../utils/db";
 
+class AuthenticationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "AuthenticationError";
+  };
+}
+
 const login = async (username: string, password: string) => {
-  try {
-    const user = await User.findOne({ where: { username: username } });
+  const user = await User.findOne({ where: { username: username } });
 
-    const isCorrect = user === null
-      ? false
-      : await bcrypt.compare(password, user.passwordHash)
+  const isCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash)
 
-    if (!(user && isCorrect)) {
-      return false;
-    }
-
-    const userToLogin = {
-      username: user.username,
-      id: user.id
-    };
-
-    const token = jwt.sign(userToLogin, process.env.SECRET || "default_secret_key");
-
-    return { token, user: username, id: user.id };
-  } catch (e) {
-    console.error(e);
-    return 0;
+  if (!(user && isCorrect)) {
+    throw new AuthenticationError("Invalid Credentials");
   }
+
+  const userToLogin = {
+    username: user.username,
+    id: user.id
+  };
+
+  const token = jwt.sign(userToLogin, process.env.SECRET || "default_secret_key");
+
+  return { token, user: username, id: user.id };
 };
 
 export default { login };
