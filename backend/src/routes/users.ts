84a05a79f73a0a9957a4ade7e6d51718bbd3c538
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-12-04 02:11:11 +0300

    Update users.ts

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -4,45 +4,45 @@ import userService from "../services/users";
 const usersRouter: express.Router = express.Router();
 
 usersRouter.get("/", async (_req: express.Request, res: express.Response) => {
-    try {
-        const users = await userService.fetchUser();
-        res.status(200).json(users);
-    } catch (e) {
-        res.status(500).json({ errMessage: e });
-    }
+  try {
+    const users = await userService.fetchUser();
+    res.status(200).json(users);
+  } catch (e) {
+    res.status(500).json({ errMessage: e });
+  }
 });
 
 usersRouter.get("/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const user = await userService.fetchUser(parseInt(id));
-        res.status(200).json(user);
-    } catch (e) {
-        console.error(e);
-        res.status(500).json({ errMessage: e });
-    }
+  try {
+    const { id } = req.params;
+    const user = await userService.fetchUser(parseInt(id));
+    res.status(200).json(user);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ errMessage: e });
+  }
 });
 
 usersRouter.post("/", async (req: express.Request, res: express.Response) => {
-    try {
-        const { username, email, name, password } = req.body;
-        const newUser = await userService.addUser(username, email, name, password);
+  try {
+    const { username, email, name, password } = req.body;
+    const newUser = await userService.addUser(username, email, name, password);
 
-        res.status(201).json(newUser);
-    } catch (e) {
-        res.status(500).json({ errMessage: e });
-    }
+    res.status(201).json(newUser);
+  } catch (e) {
+    res.status(500).json({ errMessage: e });
+  }
 
 });
 
 usersRouter.delete("/:id", async (req: express.Request, res: express.Response) => {
-    try {
-        const { id } = req.params;
-        await userService.removeUser(parseInt(id))
-        res.status(200).json({ message: "User removed successfully." });
-    } catch (e) {
-        res.status(500).json({ errMessage: e });
-    }
+  try {
+    const { id } = req.params;
+    await userService.removeUser(parseInt(id))
+    res.status(200).json({ message: "User removed successfully." });
+  } catch (e) {
+    res.status(500).json({ errMessage: e });
+  }
 });
 
 export default usersRouter;
