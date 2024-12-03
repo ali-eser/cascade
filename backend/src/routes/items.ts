c2ab4ee33c7c4c294468c0258a9cb2bf4ec3de9a
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-12-04 02:11:08 +0300

    Update items.ts

--- backend/src/routes/items.ts
+++ backend/src/routes/items.ts
@@ -4,16 +4,16 @@ import itemService from "../services/items";
 const itemsRouter: express.Router = express.Router();
 
 itemsRouter.get("/", async (_req: express.Request, res: express.Response) => {
-    try {
-        const items = await itemService.fetchItem()
-        res.status(200).json(items)
-    } catch (e) {
-        res.status(500).json({ errMessage: e });
-    }
+  try {
+    const items = await itemService.fetchItem()
+    res.status(200).json(items)
+  } catch (e) {
+    res.status(500).json({ errMessage: e });
+  }
 });
 
 itemsRouter.post("/", async (_req: express.Request, res: express.Response) => {
-    res.send("Adding new item...");
+  res.send("Adding new item...");
 });
 
 export default itemsRouter;
