2e0a3e8dbc0b284a65121322919c505f2f862702
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-12-04 02:11:21 +0300

    Update users.ts

--- backend/src/services/users.ts
+++ backend/src/services/users.ts
@@ -2,36 +2,36 @@ import bcrypt from "bcrypt";
 import { User } from "../utils/db";
 
 const fetchUser = async (id? :number) => {
-    try {
-        if (id) {
-            return await User.findByPk(id);
-        }
-        return await User.findAll({});
-    } catch (e) {
-        console.error(e);
-        return null;
+  try {
+    if (id) {
+      return await User.findByPk(id);
     }
+    return await User.findAll({});
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 };
 
 const addUser = async (username: string, email: string, name: string, password: string) => {
-    const saltRounds = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, saltRounds);
+  const saltRounds = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, saltRounds);
 
-    return await User.create({
-        username,
-        email,
-        name,
-        passwordHash
-    });
+  return await User.create({
+    username,
+    email,
+    name,
+    passwordHash
+  });
 };
 
 const removeUser = async (id :number) => {
-    try {
-        return await User.destroy({ where: { id } });
-    } catch (e) {
-        console.error(e);
-        return e;
-    }
+  try {
+    return await User.destroy({ where: { id } });
+  } catch (e) {
+    console.error(e);
+    return e;
+  }
 };
 
 export default { addUser, fetchUser, removeUser };
