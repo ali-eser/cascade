2aba8934aca818519f0ce8298519a103cb980e04
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-12-04 02:11:15 +0300

    Update items.ts

--- backend/src/services/items.ts
+++ backend/src/services/items.ts
@@ -1,15 +1,15 @@
 import { Item } from "../utils/db";
 
 const fetchItem = async (id?: number) => {
-    try {
-        if (id) {
-            return await Item.findByPk(id);
-        }
-        return await Item.findAll({});
-    } catch (e) {
-        console.error(e);
-        return null;
+  try {
+    if (id) {
+      return await Item.findByPk(id);
     }
+    return await Item.findAll({});
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 };
 
 export default { fetchItem }
